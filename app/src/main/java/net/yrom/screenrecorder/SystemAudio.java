34278bbcef7610108ce1e03ee42be21a8847bdac
Huheocter <100460265+Huheocter@users.noreply.github.com>
2025-05-17 16:32:41 +0700

    Update SystemAudio.java

--- app/src/main/java/net/yrom/screenrecorder/SystemAudio.java
+++ app/src/main/java/net/yrom/screenrecorder/SystemAudio.java
@@ -1,1 +1,16 @@
+if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+    AudioPlaybackCaptureConfiguration config = new AudioPlaybackCaptureConfiguration.Builder(mediaProjection)
+        .addMatchingUsage(AudioAttributes.USAGE_MEDIA)
+        .build();
 
+    AudioRecord record = new AudioRecord.Builder()
+        .setAudioPlaybackCaptureConfig(config)
+        .setAudioFormat(new AudioFormat.Builder()
+            .setEncoding(AudioFormat.ENCODING_PCM_16BIT)
+            .setSampleRate(44100)
+            .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
+            .build())
+        .setBufferSizeInBytes(bufferSize)
+        .build();
+    // Use 'record' for recording system audio
+}
