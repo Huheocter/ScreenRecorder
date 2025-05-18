247fdad58dbe81f1e187893eb5d52f9f33dfa631
Huheocter <100460265+Huheocter@users.noreply.github.com>
2025-05-18 09:20:23 +0700

    Update CallbackHandler.java

--- app/src/main/java/net/yrom/screenrecorder/CallbackHandler.java
+++ app/src/main/java/net/yrom/screenrecorder/CallbackHandler.java
@@ -5,41 +5,25 @@ import android.os.Looper;
 import android.os.Message;
 
 public class CallbackHandler extends Handler {
-    private static final int MSG_SUCCESS = 0;
     private static final int MSG_ERROR = 1;
-    private static final int MSG_RECORDING = 2;
 
     private final Encoder.Callback mCallback;
+    private final Encoder mEncoder;
 
-    public CallbackHandler(Encoder.Callback callback) {
+    public CallbackHandler(Encoder encoder, Encoder.Callback callback) {
         super(Looper.getMainLooper());
         mCallback = callback;
+        mEncoder = encoder;
     }
 
     @Override
     public void handleMessage(Message msg) {
-        switch (msg.what) {
-            case MSG_SUCCESS:
-                mCallback.onSuccess();
-                break;
-            case MSG_ERROR:
-                mCallback.onFailed((Throwable) msg.obj);
-                break;
-            case MSG_RECORDING:
-                mCallback.onRecording((long) msg.obj);
-                break;
+        if (msg.what == MSG_ERROR) {
+            mCallback.onError(mEncoder, (Exception) msg.obj);
         }
     }
 
-    public void notifySuccess() {
-        obtainMessage(MSG_SUCCESS).sendToTarget();
-    }
-
-    public void notifyError(Throwable error) {
+    public void notifyError(Exception error) {
         obtainMessage(MSG_ERROR, error).sendToTarget();
-    }
-
-    public void notifyRecording(long presentationTimeUs) {
-        obtainMessage(MSG_RECORDING, presentationTimeUs).sendToTarget();
     }
 }
