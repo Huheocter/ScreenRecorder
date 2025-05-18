af4cb523e13065995c3dbf5ad44622af4bd78e0d
Huheocter <100460265+Huheocter@users.noreply.github.com>
2025-05-18 09:39:56 +0700

    Create CliActivity.java

---
+++ app/src/main/java/net/yrom/screenrecorder/CliActivity.java
@@ -0,0 +1,12 @@
+package net.yrom.screenrecorder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CliActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Placeholder activity; no UI or logic
+    }
+}
